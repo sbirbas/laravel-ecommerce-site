a39e264aefedb7a764f41b115a839ab59aecb406
sbirbas <sophia@track5media.com>
2025-03-23 18:51:13 -0400

    adding some UI and backend

--- database/factories/ListingFactory.php
+++ database/factories/ListingFactory.php
@@ -3,21 +3,21 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Listing;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Listing>
- */
 class ListingFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    protected $model = Listing::class;
+
     public function definition(): array
     {
         return [
-            //
+            'title' => fake()->sentence(3),
+            'description' => fake()->paragraph(),
+            'img' => fake()->imageUrl(),
+            'price' => fake()->randomFloat(2, 10, 1000),
+            'vendor' => fake()->company(),
+            'stock' => fake()->numberBetween(1, 500),
         ];
     }
 }
---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
+        @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+            @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @else
+
+        @endif
+    </head>
+<body>
+
+    @include('partials.navbar')
+
+    <div class="container">
+        @yield('content')
+    </div>
+
+    @include('partials.footer')
+
+</body>
+</html>
